bf284acaff9cf98d59acb588a0aba86dd3977217
eva lampin <eva.lampin@hotmail.fr>
2022-09-06 18:13:26 +0200

    adding bootstrp and wcs code

--- app/controllers/argonautes_controller.rb
+++ app/controllers/argonautes_controller.rb
@@ -4,10 +4,23 @@ class ArgonautesController < ApplicationController
   end
 
   def new
+    @argonaute = Argonaute.new
   end
 
   def create
-    @argonaute = Argonaute.new(params[:argonaute])
+    @argonaute = Argonaute.new(argonaute_params)
     @argonaute.save
+    if @argonaute.save
+      render :new, notice: "L'Argonaute a bien été ajouté !"
+    else
+      render :new, notice: "Echec ! Cet Argonaute n'a pas pu être enregistré"
+    end
   end
+
+  private
+
+  def argonaute_params
+    params.require(:argonaute).permit(:name)
+  end
+
 end
