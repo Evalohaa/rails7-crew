4857eb7ec5566fa62051fc3155dc1da5568b0853
eva lampin <eva.lampin@hotmail.fr>
2022-09-06 18:30:32 +0200

    delete the changes

---
+++ app/controllers/argonautes_controller.rb
@@ -0,0 +1,26 @@
+class ArgonautesController < ApplicationController
+  def index
+    @argonautes = Argonaute.all
+  end
+
+  def new
+    @argonaute = Argonaute.new
+  end
+
+  def create
+    @argonaute = Argonaute.new(argonaute_params)
+    @argonaute.save
+    if @argonaute.save
+      redirect_to root_path, notice: "L'Argonaute a bien été ajouté !"
+    else
+      render :new, notice: "Echec ! Cet Argonaute n'a pas pu être enregistré"
+    end
+  end
+
+  private
+
+  def argonaute_params
+    params.require(:argonaute).permit(:name)
+  end
+
+end
